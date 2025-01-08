2f36acb4723ec8dfe396c36be0cd1f26f580667c
AshikurZaman01 <ashikurzaman774@gmail.com>
2025-01-08 16:39:45 +0600

    add AllProjects Ui

--- Frontend/src/Components/Pages/Home/Home.jsx
+++ Frontend/src/Components/Pages/Home/Home.jsx
@@ -1,17 +1,41 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { UserContext } from "../../../ContextApi/user.context";
 import ModalForm from "./ModalForm";
 import HomeText from "./HomeText";
 import axiosInstance from "../../../Config/Axios";
+import AllProjects from "../Projects/AllProjects";
 
 const Home = () => {
 
     const { user } = useContext(UserContext);
 
+
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [projectName, setProjectName] = useState("");
     const [errorMessage, setErrorMessage] = useState("");
     const [loading, setLoading] = useState(false);
+    const [project, setProject] = useState([]);
+
+
+    useEffect(() => {
+        const getAllProjects = async () => {
+            setLoading(true);
+
+            try {
+                const response = await axiosInstance.get("/projects/getAllProjects");
+                setProject(response.data);
+            }
+            catch (error) {
+                console.log(error);
+            }
+            finally {
+                setLoading(false);
+            }
+        };
+
+        getAllProjects();
+    }, []);
+
 
 
     const handleCreateProject = async (e) => {
@@ -27,13 +51,11 @@ const Home = () => {
         setLoading(true);
         try {
             const response = await axiosInstance.post('projects/projectCreate', { name: projectName });
-
             if (response.data.message) {
                 alert(response.data.message);
                 setProjectName('');
                 setIsModalOpen(false);
             }
-
 
         } catch (error) {
             console.log(error)
@@ -41,17 +63,20 @@ const Home = () => {
         } finally {
             setLoading(false);
         }
-
     }
 
 
     return (
-        <main className="flex   h-screen bg-gray-900 text-gray-100">
-
-
+        <main className="flex h-screen bg-gray-900 text-gray-100">
 
             <div>
-                <HomeText setIsModalOpen={setIsModalOpen}></HomeText>
+                {
+                    project.length > 0 ?
+                        <HomeText setIsModalOpen={setIsModalOpen}></HomeText>
+                        :
+                        <AllProjects></AllProjects>
+                }
+
 
                 <ModalForm
                     isModalOpen={isModalOpen}
---
+++ Frontend/src/Components/Pages/Projects/AllProjects.jsx
@@ -0,0 +1,15 @@
+
+const AllProjects = () => {
+    return (
+        <div className=" w-[200px] h-screen border-r border-gray-800">
+
+            <div className="text-center my-5">
+                <h2 className="text-3xl capitalize font-semibold">User List</h2>
+                <hr className="h-1 bg-gray-800 border-none mt-2" />
+            </div>
+
+        </div>
+    )
+}
+
+export default AllProjects
